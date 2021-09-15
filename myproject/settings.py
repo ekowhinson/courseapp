fed0f13931c612d8f0b3d6600562e3038de03528
ekowhinson <ekowhinson@gmail.com>
2021-09-15 17:56:44 +0000

    models update

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'MTIxMDgtZWtvd2hp',
+        'PASSWORD': 'MjM4Ny1la293aGlu',
         'HOST': 'localhost',
         'PORT': '5432',
     }
